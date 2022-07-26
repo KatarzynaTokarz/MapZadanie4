21e4ba5eb6cbc213f5ed92860c1350d5b9dfb964
Uzytkownik <uzytkownik@kursGIT.pl>
2022-07-26 15:53:32 +0200

    third commit

--- MapZadanie4.cpp
+++ MapZadanie4.cpp
@@ -1,42 +1,43 @@
 #include <iostream>
 #include <map>
 
+void printCounter(std::string zdanie)
+{
+    std::map<char, int> counter;
+
+    for (int i = 0; i < zdanie.size(); ++i)
+    {
+        if (counter.find(zdanie[i]) != counter.end()) {
+            counter[zdanie[i]] = counter[zdanie[i]]++;
+        }
+        else {
+            //omijam spacje
+            if (zdanie[i] != ' ')
+            {
+                counter[zdanie[i]] = 1;
+            }
+        }
+    }
+    for (auto& it : counter)
+    {
+        std::cout << it.first << " = " << it.second << std::endl;
+    }
+    std::cout << std::endl;
+
+    for (auto kv : counter)
+    {
+        auto& key = kv.first;
+        auto& value = kv.second;
+
+        std::cout << key << " = " << value << std::endl;
+    }
+}
+
 int main()
 {
-   std::string zdanie = "ala ma kota";
-
-   std::map<char, int> counter;
-
-   for (int i = 0; i < zdanie.size(); ++i)
-   {
-       if (counter.find(zdanie[i]) != counter.end()) {
-           counter[zdanie[i]] = counter[zdanie[i]]++;
-       }
-       else {
-           //omijam spacje
-           if (zdanie[i] != ' ')
-           {
-               counter[zdanie[i]] = 1; 
-           }
-       }
-   }
-
-   for (auto& it : counter)
-   {
-       std::cout << it.first << " = " << it.second << std::endl;
-   }
-
-   std::cout << std::endl;
-
-
-   for (auto kv : counter)
-   {
-       auto& key = kv.first;
-       auto& value = kv.second;
-
-
-       std::cout << key << " = " << value << std::endl;
-   }
-
+    std::string tekst; 
+    std::cout << "Podaj tekst do przetestowania" << std::endl;
+    std::cin >> tekst;
+    printCounter(tekst);
 }
 
